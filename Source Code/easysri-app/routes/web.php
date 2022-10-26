65cbfe912f0809d8fce66e6d9ed1e570e74f5e0a
IT20230692 <IT20230692@my.sliit.lk>
2022-10-26 19:20:38 +0530

    create userdashboard

--- Source Code/easysri-app/routes/web.php
+++ Source Code/easysri-app/routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\UserPagesController;
 
 //Admin-Dashboard
 Route::get('/admindashboard', [AdminDashboardController::class, 'dashboardview'])->name('admindashboard');
+
+
+
+
+
+
+
+
+//User Dashboard
+Route::get('/userdashboard', [UserPagesController::class, 'indexuserdashboard'])->name('userdashboard');
