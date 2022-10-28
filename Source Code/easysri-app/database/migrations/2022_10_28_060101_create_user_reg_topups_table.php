25bf6c55c1e39b6456941e9dc6344afd48f810a2
IT20230692 <IT20230692@my.sliit.lk>
2022-10-28 17:23:49 +0530

    add model to reg user

---
+++ Source Code/easysri-app/database/migrations/2022_10_28_060101_create_user_reg_topups_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_reg_topups', function (Blueprint $table) {
+            $table->id('userregid');
+            $table->string('Easysrino');
+            $table->string('Fname');
+            $table->string('Lname');
+            $table->string('NIC');
+            $table->string('Address');
+            $table->string('City');
+            $table->string('Zipcode');
+            $table->string('MobNo');
+            $table->double('Initalpay');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_reg_topups');
+    }
+};
