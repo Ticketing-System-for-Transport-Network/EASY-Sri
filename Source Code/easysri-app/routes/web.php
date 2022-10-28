ec1bbbd6a5aead1d66b11d01130b4e1d115c122b
RCP Rajapaksha <it20137250@my.sliit.lk>
2022-10-28 10:45:23 +0530

    Create bus timetable table

--- Source Code/easysri-app/routes/web.php
+++ Source Code/easysri-app/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminDashboardController;
 use App\Http\Controllers\BusTimeInsertController;
+use App\Http\Controllers\BusTimeViewController;
 
 //Admin-Dashboard
 Route::get('/admindashboard', [AdminDashboardController::class, 'dashboardview'])->name('admindashboard');
@@ -11,3 +12,5 @@ Route::prefix('/bustimeinsert')->group(function () {
     Route::get('/', [BusTimeInsertController::class, "bustimeinsertview"])->name('bustimeinsert');
     Route::post('/store', [BusTimeInsertController::class, "store"])->name('bustimeinsert.store');
 });
+
+Route::get('/bustimeview', [BusTimeViewController::class, 'busview'])->name('bustimeview');
